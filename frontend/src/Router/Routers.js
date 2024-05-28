84ff59006822ecc95009f91596de5e9036846c0f
Jerroshin <158449174+Webhosting123@users.noreply.github.com>
2024-05-28 18:06:19 +0530

    homeUpdate

--- frontend/src/Router/Routers.js
+++ frontend/src/Router/Routers.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import { Route,Routes } from 'react-router-dom';
-import Home from '../Pages/Home/Home';
-import About from '../Pages/Aboutus/About';
-import Discover from '../Pages/Discover/Discover';
-import Service from '../Pages/Service/Service';
-import Career from '../Pages/Career/Career';
-import Contact from '../Pages/Contact/Contact';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../Pages/Home/Home";
+import About from "../Pages/Aboutus/About";
+import Discover from "../Pages/Discover/Discover";
+import Service from "../Pages/Service/Service";
+import Career from "../Pages/Career/Career";
+import Contact from "../Pages/Contact/Contact";
 
 const Routers = () => {
   return (
     <>
-
-    <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/discover' element={<Discover/>}/>
-        <Route path='/service' element={<Service/>}/>
-        <Route path='/career' element={<Career/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/career" element={<Career />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default Routers
+export default Routers;

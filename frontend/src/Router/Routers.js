bf9fb0c76d3c9a1083fb8a28266bafbfc82220e5
rishii64 <saptarsismmitra@gmail.com>
2024-05-29 15:16:20 +0530

    updated service-page and navbar

--- frontend/src/Router/Routers.js
+++ frontend/src/Router/Routers.js
@@ -1,12 +1,11 @@
 
-import React, { Suspense } from 'react'
+import React from 'react'
 import { Route,Routes } from 'react-router-dom';
 import Home from '../Pages/Home/Home';
 import About from '../Pages/Aboutus/About';
 import Discover from '../Pages/Discover/Discover';
 import Career from '../Pages/Career/Career';
 import Contact from '../Pages/Contact/Contact';
-import LogoLoader from '../Component/Logo_Loader/LogoLoader'
 import ProductData from '../Pages/Discover/ProductData';
 import ProductPage from '../Pages/Discover/ProductPage'
 
@@ -18,6 +17,7 @@ const Routers = () => {
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/discover' element={<Discover/>}/>
+        <Route path='/discover/:id' element={<ProductPage/>}/>
         <Route path='/career' element={<Career/>}/>
         <Route path='/contact' element={<Contact/>}/>
       </Routes>

3cae8cb3b82a51d054f44324d005e76772bad8a2
Ranjan Kumar Chanda <118096113+Ranjan-coder@users.noreply.github.com>
2024-05-29 17:44:58 +0530

    Revert "Nave bar upadate"

--- frontend/src/Component/Navbar/Navbar.js
+++ frontend/src/Component/Navbar/Navbar.js
@@ -1,44 +1,6 @@
 import React from "react";
-
-
-import Container from "react-bootstrap/Container";
-
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-
-import Offcanvas from "react-bootstrap/Offcanvas";
-
-import logo from "../../Assets/officiallogo.png";
-
-function NavBar() {
-  return (
-    <Navbar expand="lg" className="bg-body-tertiary mb-3">
-      <Container fluid>
-        <Navbar.Brand href="#">
-          <img src={logo} alt="" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="offcanvasNavbar" />
-        <Navbar.Offcanvas
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-          placement="end"
-        >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title id="offcanvasNavbarLabel">
-              Mackinlay
-            </Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="justify-content-end flex-grow-1 pe-3">
-              <Nav.Link href="#About">ABOUT US</Nav.Link>
-              <Nav.Link href="#link">DISCOVER</Nav.Link>
-              <Nav.Link href="#link">CAREER</Nav.Link>
-              <Nav.Link href="#link">CONTACT US</Nav.Link>
-            </Nav>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
-      </Container>
-    </Navbar>
+import { NavLink } from "react-router-dom";
+import Navbar_css from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
@@ -70,8 +32,7 @@ const Navbar = () => {
         <div className={Navbar_css.widthlink}></div>
       </div>
     </>
-
   );
-}
+};
 
-export default NavBar;
+export default Navbar;

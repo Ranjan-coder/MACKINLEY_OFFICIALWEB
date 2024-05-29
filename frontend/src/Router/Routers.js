566b5867bd546a2f0efc7cef23e46126cd0d94e5
rishii64 <saptarsismmitra@gmail.com>
2024-05-29 12:08:37 +0530

    updated

--- frontend/src/Router/Routers.js
+++ frontend/src/Router/Routers.js
@@ -6,7 +6,6 @@ import ProductPage from '../Pages/Discover/ProductPage';
 const Home = lazy(() => import("../Pages/Home/Home"))
 const About = lazy(() => import("../Pages/Aboutus/About"))
 const Discover = lazy(() => import("../Pages/Discover/Discover"))
-// const Service = lazy(() => import("../Pages/Service/Service"))
 const Career = lazy(() => import("../Pages/Career/Career"))
 const Contact = lazy(() => import("../Pages/Contact/Contact"))
 
@@ -19,7 +18,6 @@ const Routers = () => {
           <Route path="/about" element={<Suspense fallback={<LogoLoader />}> <About /> </Suspense>} />
           <Route path="/discover" element={<Suspense fallback={<LogoLoader />}> <Discover /> </Suspense>} />
           <Route path="/discover/:id" element={<Suspense fallback={<LogoLoader />}> <ProductPage /> </Suspense>} />
-          {/* <Route path="/service" element={<Suspense fallback={<LogoLoader />}> <Service /> </Suspense>} /> */}
           <Route path="/career" element={<Suspense fallback={<LogoLoader />}> <Career /> </Suspense>} />
           <Route path="/contact" element={<Suspense fallback={<LogoLoader />}> <Contact /> </Suspense>} />
         </Routes>

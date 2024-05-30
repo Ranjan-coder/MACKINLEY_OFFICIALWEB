5945a22ec36430c672ed2f6567e7a3bbc06bbfd6
Ranjan-coder <ranjanchanda06@gmail.com>
2024-05-30 16:01:21 +0530

    navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,13 @@
 import "./App.css";
 import Routers from "./Router/Routers";
-import officiallogo from "./Assets/officiallogo.png";
 import Navbar from "./Component/Navbar/Navbar";
-import { useNavigate } from "react-router-dom";
 import Footer from "./Component/Footer/Footer";
 
 function App() {
-  const NavigateTo = useNavigate();
   return (
     <>
-      <div className="navbarlayout">
-        <div>
-          <img
-            className="officiallogo"
-            onClick={() => NavigateTo("/")}
-            src={officiallogo}
-            alt="officiallogo"
-          />
-        </div>
-        <div className="navbarroutes">
-          <Navbar />
-        </div>
-      </div>
+        <Navbar />
+
       <Routers />
       <Footer />
     </>

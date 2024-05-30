abc9fcf0ef0919070723db18c24a788c43607152
Jerroshin <jerroshin5@gmail.com>
2024-05-30 18:28:28 +0530

    newNavbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import Routers from "./Router/Routers";
-import Navbar from "./Component/Navbar/Navbar";
+import Navebar from "./Component/Navbar/Navbar";
 import Footer from "./Component/Footer/Footer";
 
 function App() {
   return (
     <>
-        <Navbar />
+      <Navebar />
 
       <Routers />
       <Footer />

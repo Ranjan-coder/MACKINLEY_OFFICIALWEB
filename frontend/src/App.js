658cbbf1fef6362b6bde3c5814941f3032c52a8d
Jerroshin <jerroshin5@gmail.com>
2024-06-01 09:58:26 +0530

    mainPageNew

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import Routers from "./Router/Routers";
 import Navebar from "./Component/Navbar/Navbar";
 import Footer from "./Component/Footer/Footer";
+import ScrollToTop from "./Component/scroll_top/scroll";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Navebar />
-
       <Routers />
       <Footer />
     </>
---
+++ frontend/src/Pages/Home/Data.js
@@ -0,0 +1,33 @@
+import Industry from "../../Assets/industry.jpeg";
+import Customer from "../../Assets/customer.jpeg";
+import Exc from "../../Assets/exc.jpeg";
+import Dig from "../../Assets/dig.jpeg";
+
+const Data = [
+  {
+    Image: Industry,
+    Title: "Industry Expertise",
+    Content:
+      "Our deep understanding of the BFSI sector sets us apart. We identify industry trends and challenges, enabling us to offer solutions that are both innovative and practical.",
+  },
+  {
+    Image: Customer,
+    Title: "Customer-Centric Approach",
+    Content:
+      "At Mackinlay, our clients are at the center of everything we do. We take the time to understand your business, challenges, and goals. This approach ensures our solutions are perfectly aligned with your needs.",
+  },
+  {
+    Image: Exc,
+    Title: "Commitment to Excellence",
+    Content:
+      "We are dedicated to delivering excellence in every project. Our rigorous quality assurance processes ensure our solutions meet the highest standards of performance, security, and reliability.",
+  },
+  {
+    Image: Dig,
+    Title: "Path to Digital Excellence",
+    Content:
+      "Partner with Mackinlay and embark on a journey of digital excellence. Let us help you harness the power of innovation to transform your business, enhance your competitive edge, and achieve your strategic objectives.",
+  },
+];
+
+export default Data;

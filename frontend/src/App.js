cce89528325fe4ad37ce5b9496842a2d8eb99a77
Jerroshin <jerroshin5@gmail.com>
2024-06-01 14:16:46 +0530

    scrollUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,12 @@ import Routers from "./Router/Routers";
 import Navebar from "./Component/Navbar/Navbar";
 import Footer from "./Component/Footer/Footer";
 import { Toaster } from "react-hot-toast";
+import ScrollToTop from "./Component/scroll_top/scroll";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Toaster />
       <Navebar />
       <Routers />
